66b416fa0b3cae4e7d8d981e27612a6f6c6e118d
MatteoNorr <matteo.norrito@outlook.com>
2023-08-03 15:34:52 +0200

    feat: first setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    additionalData: `
+      @import "./src/styles/variables.scss";
+      @import "./src/styles/mixins.scss";
+      `,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
